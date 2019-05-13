4fbc18f59f1e7273f32454bc44302752a09b7720
Yuta Shimakawa <bananaumai@tinkermode.com>
2019-05-13 14:34:31 -0700

    implement basic activity flow

--- app/src/main/java/dev/bananaumai/android/practices/kvstore/MainActivity.kt
+++ app/src/main/java/dev/bananaumai/android/practices/kvstore/MainActivity.kt
@@ -1,12 +1,25 @@
 package dev.bananaumai.android.practices.kvstore
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private val tag = this::class.java.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        val txt = findViewById<EditText>(R.id.password).text
+        Log.d(tag, "password is $txt")
+        Intent(this, DisplayPasswordActivity::class.java).let { intent ->
+            startActivity(intent)
+        }
     }
 }
